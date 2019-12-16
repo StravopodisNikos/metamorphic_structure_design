0b82035140aaa67818dea9652a6ab085caa719b8
StravopodisNikos <stravnik.rob@gmail.com>
2019-12-16 16:49:19 +0200

    Evaluates structures extracted from ga in moul2

---
+++ FinalStructureEvaluation.m
@@ -0,0 +1,211 @@
+function [check] = FinalStructureEvaluation(xi_best,structure2check,strucureURDFfile)
+% Evaluates structures extracted from MOUL2 optimization files:
+% 1. Investigate3DoF_MassMatrixRAAD2020.m
+% 2. Investigate3DoF_MassMatrixRAAD2020_NoPseudo.m
+% --- Rules of the above files are considered executed! ---
+
+% Call
+
+% Takes best ga structure and evaluates kinematics and TCP Mass Ellipsoid
+% Condition number is plotted in C-space of reference anatomy only!
+%% load libraries
+% load Murray kinematics
+addpath('/home/nikos/matlab_ws/kinematics/robotlinks')
+addpath('/home/nikos/matlab_ws/kinematics/screws') 
+addpath('/home/nikos/matlab_ws/kinematics/util')
+% load Js-Jb calculation function
+addpath('/home/nikos/matlab_ws/project_ABBpaper/matlab_files')
+% load main folder
+addpath('/home/nikos/matlab_ws/modular_dynamixel/')
+addpath('/home/nikos/matlab_ws/modular_dynamixel/structure_synthesis')
+addpath('/home/nikos/matlab_ws/modular_dynamixel/raad2020')
+
+%% load zero data for STRUCTURAL BLOCKS
+% data are obtained by .m files:
+% modular_dynamixel/structural_synthesis/kinematic_verification_C01.m for SB01
+% modular_dynamixel/structural_synthesis/kinematic_verification_C011.m for SB011
+load('SB10_zero_data_CoM.mat');
+pi_10 = pi(:,2:4); % j-i1-k(!)
+wi_10 = wi(:,2:3); % j-i1
+g0_10 = g0;
+gsc_10 = g0(:,:,9); % g_s_lk0
+g_lk_TOOL_10 = g0(:,:,10);
+xi_10 = xi;
+M_10_b = M0_CoM; %6x6x2
+load('SB110_zero_data_CoM.mat');
+pi_110 = pi(:,2:5); % j1-j2-i1-k(!)
+wi_110 = wi(:,2:4); % j1-j2-i1
+g0_110 = g0;
+gsc_110 = g0(:,:,2); % g_s_lk0
+g_lk_TOOL_110 = g0(:,:,14);
+xi_110 = xi;
+M_110_b = M0_CoM;
+%% Initialization of structure geometry
+t0 = 0; % first active angle
+
+p1_0 = [0 0 0.0570]';
+w1_0 = [0 0 1]';
+x1 = createtwist(w1_0,p1_0); %ξa1
+% zero tf to the first active frame
+g_s_li1_0 = [   1.0000      0         0         0;...
+                 0          1.0000    0         0;...
+                 0          0         1.0000    0.0570;...
+                 0          0         0         1.0000];
+% zero tf to the first connection Point of Synthetic Joint
+% (zero tf between {s} -> frame
+g_s_lk1_0 = [1.0000    0         0          0;...
+             0         1.0000    0          0;...
+             0         0         1.0000     0.0570;...
+             0         0         0          1.0000];
+exp1 = twistexp(x1,t0);
+f1 = 'pi'; v1 = p1_0;
+f2 = 'wi'; v2 = w1_0;
+f3 = 'g0'; v3 = g_s_li1_0;
+f4 = 'Cg'; v4 = g_s_lk1_0; % only for this, because the first!
+f5 = 'expi'; v5 = exp1;
+f6 = 'xi'; v6 = x1;
+f7 = 'Sframe'; v7 = zeros(4);
+f8 = 'gsli1'; v8 = g_s_li1_0;
+s1 = struct(f1,v1,f2,v2,f3,v3,f4,v4,f5,v5,f6,v6,f7,v7,f8,v8);
+% For second string part a check if '010' or '0110' takes place
+% f5-f6-f7 are only initialization since 10,110 never start! Values are
+% given ONLY inside functions
+f1 = 'pi'; v1 = pi_10; 
+f2 = 'wi'; v2 = wi_10;
+f3 = 'g0'; v3 = g0_10;
+f4 = 'Cg'; v4 = g_lk_TOOL_10;
+f5 = 'expi'; v5 = exp1; 
+f6 = 'xi'; v6 = x1;
+f7 = 'Sframe'; v7 = zeros(4);
+f8 = 'Mbi'; v8 = M_10_b;
+s2 = struct(f1,v1,f2,v2,f3,v3,f4,v4,f5,v5,f6,v6,f7,v7,f8,v8);
+% For third string part a check if '10' or '110' takes place
+% f5-f6-f7 are only initialization since 10,110 never start! Values are
+% given ONLY inside functions
+f1 = 'pi'; v1 = pi_110; 
+f2 = 'wi'; v2 = wi_110;
+f3 = 'g0'; v3 = g0_110;
+f4 = 'Cg'; v4 = g_lk_TOOL_110;
+f5 = 'expi'; v5 = exp1;
+f6 = 'xi'; v6 = x1;
+f7 = 'Sframe'; v7 = zeros(4);
+f8 = 'Mbi'; v8 = M_110_b;
+s3 = struct(f1,v1,f2,v2,f3,v3,f4,v4,f5,v5,f6,v6,f7,v7,f8,v8);
+
+%% Get best chromosome values
+R_1 = xi_best(1:3);
+P_1 = xi_best(4:6);
+R_2 = xi_best(7:9);
+P_2 = xi_best(10:12);
+%% Dependng on structure string entered, initialize pseudoangles to ref
+k = 1; % counter of pseudojoints in structure
+for i = 1:length(structure2check)
+    if structure2check(i) == '1'
+        tp(k) = 0;
+        k = k+1;
+    end
+end
+
+%% Kinematics Evaluation
+switch structure2check
+    case '0110110'
+%% =========================     S0110110     ===========================
+        %% A. First kinematics are calculated through MATLAB solver
+        % robot urdf is built from /structure_synthesis/S0110110.xacro
+        % given manually the xi values, FOR ZERO PSEUDO ANGLES!!!!
+        [robotS0110110] = importrobot(strucureURDFfile);  
+        robotS0110110.DataFormat = 'column';
+        robotS0110110.Gravity = [0 0 -9.80665];
+        robotS0110110_figure = figure;
+        %% Show reference C01
+        figure(robotS0110110_figure);
+        config = [0 0 0]';
+        show(robotS0110110,config,'PreservePlot',false);
+        hold on;
+        axis auto;
+        box on;
+        %% B. Here my kinematics solver is used (the one used in ga)
+        n = 2; % Structural Block number
+        [S2] = Build_SB110_forGMinMaxDCI(s1,s3,R_1,P_1,n);
+        xi_ai(:,1) = s1.xi;
+        xi_ai(:,2) = S2.xi(:,3);
+        xi_pi(:,1) = S2.xi(:,1);
+        xi_pi(:,2) = S2.xi(:,2);
+        %gst0 = S2.Cg;
+        gsCoM10 = S2.g_s_CoM0;
+
+        n = 3; % Structural Block number
+        [S3] = Build_SB110_forGMinMaxDCI(S2,s3,R_2,P_2,n);
+        %xi_ai(:,1) = s1.xi;
+        xi_ai(:,3) = S3.xi(:,3);
+        xi_pi(:,3) = S3.xi(:,1);
+        xi_pi(:,4) = S3.xi(:,2);
+        gst0 = S3.Cg;
+        gsCoM20 = S3.g_s_CoM0;
+
+        gsCoMi0(:,:,1) = gsCoM10(:,:,1);
+        gsCoMi0(:,:,2) = gsCoM20(:,:,1);
+        % gsCoMi0(:,:,3) = gsCoM20(:,:,2);
+        gsCoMi0(:,:,3) = S3.Cg; % approximation
+
+        Mbi(:,:,1) = s2.Mbi(:,:,1);
+        Mbi(:,:,2) = s2.Mbi(:,:,1);
+        Mbi(:,:,3) = s2.Mbi(:,:,2);
+        %% Sets current anatomy pseudo exponential
+        P1 = twistexp(xi_pi(:,1),tp(1))*twistexp(xi_pi(:,2),tp(2));
+        P2 = twistexp(xi_pi(:,3),tp(3))*twistexp(xi_pi(:,4),tp(4));
+        Pi(:,:,1) = P1;
+        Pi(:,:,2) = P2;
+        
+        %% Evaluation is executed at random configuration!
+        qrand = config;
+        
+        % 1. FKP
+        exp_ai(:,:,1) = twistexp(xi_ai(:,1),qrand(1));
+        exp_ai(:,:,2) = twistexp(xi_ai(:,2),qrand(2));
+        exp_ai(:,:,3) = twistexp(xi_ai(:,3),qrand(3));
+        gst = exp_ai(:,:,1)*Pi(:,:,1)*exp_ai(:,:,2)*Pi(:,:,2)*exp_ai(:,:,3)*gst0;
+        % 2. Js,Jb
+        Js(:,1) = xi_ai(:,1);
+        Js(:,2) = ad(exp_ai(:,:,1)*Pi(:,:,1))*xi_ai(:,2);
+        Js(:,3) = ad(exp_ai(:,:,1)*Pi(:,:,1)*exp_ai(:,:,2)*Pi(:,:,2))*xi_ai(:,3);
+        figure(robotS0110110_figure)
+        plot_xi2 = drawtwist(Js(:,2)); hold on;
+        plot_xi3 = drawtwist(Js(:,3)); hold on;
+        % Jbody
+        Jb(:,1) = ad(inv(exp_ai(:,:,1)*Pi(:,:,1)*exp_ai(:,:,2)*Pi(:,:,2)*exp_ai(:,:,3)*gst0))*xi_ai(:,1);
+        Jb(:,2) = ad(inv(exp_ai(:,:,2)*Pi(:,:,2)*exp_ai(:,:,3)*gst0))*xi_ai(:,2);
+        Jb(:,3) = ad(inv(exp_ai(:,:,3)*gst0))*xi_ai(:,3);
+        
+        % 3. Mb
+        % Compute Link CoM Jacobians
+        for i=1:3
+            [Jbsli(:,:,i)] = Jbody_CoM_3DoF_forDCI(xi_ai, exp_ai, Pi, gsCoMi0, i);
+        end
+        % Compute mass matrix
+        [Mb] = CalculateOnlyBodyMassMatrix(Jbsli,Mbi);
+        
+        % 4. Ellipsoid
+        [Meig,Keig] = PlotMassEllipsoid_3D_atTCP(Mb,Jb,gst,robotS0110110_figure)
+        
+        disp('Structure S0110110 was evaluated.')
+        check = 'ok';
+%% =========================     S0110110     ===========================
+    case '01010'
+%% =========================      S01010      ===========================
+
+%% =========================      S01010      ===========================
+    case '010110'
+%% =========================      S010110     ===========================
+
+%% =========================      S010110     ===========================
+    case '011010'
+%% =========================      S011010     ===========================
+
+%% =========================      S011010     ===========================        
+    otherwise
+        disp('Structure given is not accepted.')
+        check = 'error';  
+end
+
+end
